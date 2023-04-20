4b145b0a74c3a13dc7822f13c4eee4d47d61c2fb
fadhil3310 <80736446+fadhil3310@users.noreply.github.com>
2023-04-20 17:31:40 +0700

    Update setup-android-sdk.sh

--- scripts/setup-android-sdk.sh
+++ scripts/setup-android-sdk.sh
@@ -12,6 +12,22 @@ echo "Start installing openjdk"
 sudo apt-get install openjdk-17-jre
 echo "Finished installing openjdk"
 
+uname -p
+
+if [ uname -p = "x86_64" ]; then
+	$JAVA_HOME = /usr/lib/jvm/java-17-openjdk-amd64
+elif [ uname -p = "i686" ]; then
+	$JAVA_HOME = /usr/lib/jvm/java-17-openjdk-i686
+elif [ uname -p = "aarch64" ]; then
+	$JAVA_HOME = /usr/lib/jvm/java-17-openjdk-aarch64
+elif [ uname -p = "arm" ]; then
+	$JAVA_HOME = /usr/lib/jvm/java-17-openjdk-arm
+else
+	echo "Unknown processor type"
+	exit 1
+fi
+
+
 # Install desired parts of the Android SDK:
 . $(cd "$(dirname "$0")"; pwd)/properties.sh
 . $(cd "$(dirname "$0")"; pwd)/build/termux_download.sh
