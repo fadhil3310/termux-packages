46ee57bc8d44364b262895fba46f2e06435b7586
fadhil3310 <80736446+fadhil3310@users.noreply.github.com>
2023-04-20 17:13:43 +0700

    Update setup-android-sdk.sh

--- scripts/setup-android-sdk.sh
+++ scripts/setup-android-sdk.sh
@@ -4,6 +4,12 @@ set -e -u
 
 : "${TERMUX_PKG_TMPDIR:="/tmp"}"
 
+# Tambahan, install openjdk dulu biar ga error:
+# ERROR: JAVA_HOME is set to an invalid directory: /usr/lib/jvm/java-8-openjdk-amd64
+# Please set the JAVA_HOME variable in your environment to match the
+# location of your Java installation.
+sudo apt-get install openjdk-8-jre
+
 # Install desired parts of the Android SDK:
 . $(cd "$(dirname "$0")"; pwd)/properties.sh
 . $(cd "$(dirname "$0")"; pwd)/build/termux_download.sh
