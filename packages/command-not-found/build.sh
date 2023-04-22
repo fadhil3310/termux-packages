7330f69ef9a6321e5b9ee9a0a794162d1787bcf2
fadhil3310 <80736446+fadhil3310@users.noreply.github.com>
2023-04-22 09:08:41 +0700

    Update command-not-found

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -1,11 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
+TERMUX_PKG_HOMEPAGE=https://github.com/fadhil3310/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.1.0
 TERMUX_PKG_REVISION=19
-TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=e8fe565d7385a682074e42960a04c4a76d10998fab1638e4b8971a8f8060c026
+TERMUX_PKG_SRCURL=https://github.com/fadhil3310/command-not-found/archive/refs/tags/Release.tar.gz
+TERMUX_PKG_SHA256=897c3e3e60739cc8a700db0370cb70dae8220fb88c9bcdc4f21f995d0c4757a4
 TERMUX_PKG_DEPENDS="libc++"
 
 termux_step_pre_configure() {
